980833346d9411b4d7027ff9d032b2c59bc70013
Khaya Nojoko <knojoko@gmail.com>
2024-04-21 21:23:30 +0200

    Made changes and added folders for each topic/project

--- Main.java
+++ Main.java
@@ -4,6 +4,14 @@ public class Main {
 	{
 
 		System.out.println("HelloWorld!");
+	
+		int b = 6;
+		int c = 7;
+		int x = 9;
+
+		System.out.println( b * c + x );
 	}
+
 }
+
 
---
+++ practice/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		// This for practice for java code.
+		System.out.println("This is for Practising.");
+	}
+
+}	
