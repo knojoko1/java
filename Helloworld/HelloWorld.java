5593edf0ca8fa7b296736bb482f4a20cd4d0d741
Khayalethu Prudent Nojoko <knojoko@gmail.com>
2025-03-11 12:01:54 +0200

    Created package and moved around files and created main file to run the code

--- Helloworld/HelloWorld.java
+++ Helloworld/HelloWorld.java
@@ -1,8 +1,9 @@
+package Helloworld;
+
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void printHelloWorld() {
         System.out.println("Hello, World!");
-
     }
 
 }
---
+++ Main.java
@@ -0,0 +1,10 @@
+import Helloworld.HelloWorld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HelloWorld.printHelloWorld();
+
+    }
+
+}
